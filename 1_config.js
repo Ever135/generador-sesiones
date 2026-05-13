d184d5ec471a1a18a38b3217c7988ed9802b53e7
Ever135 <ebautistah.22@ispa.edu.pe>
2026-05-12 20:40:16 -0500

    Update 1_config.js

--- 1_config.js
+++ 1_config.js
@@ -1,1 +1,42 @@
 
+// 1_config.js
+
+const COLORS = {
+  MAIN: "1B3A6B",
+  ACC: "C0392B",
+  GREEN: "1A6B1A",
+  PURP: "6A1B9A",
+  GOLD: "7A5200",
+  GRAY: "555555"
+};
+
+const FONT = {
+  FAMILY: "Arial",
+  SIZE_BASE: 20,
+  SIZE_TITLE: 32,
+  SIZE_SUBTITLE: 24
+};
+
+const SPACING = {
+  SMALL: 40,
+  MEDIUM: 80,
+  LARGE: 160
+};
+
+const BORDERS = {
+  THIN: 2,
+  NORMAL: 4,
+  THICK: 8
+};
+
+const TABLE = {
+  WIDTH: 9360
+};
+
+module.exports = {
+  COLORS,
+  FONT,
+  SPACING,
+  BORDERS,
+  TABLE
+};
